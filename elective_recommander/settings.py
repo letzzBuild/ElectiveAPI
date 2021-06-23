24a247097098d4f9cbf857970fd585708081d3b4
letzzBuild <letzzBuild@gmail.com>
2021-06-23 18:30:59 +0530

    update heroku 2

--- elective_recommander/settings.py
+++ elective_recommander/settings.py
@@ -168,7 +168,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ############ STATIC FILS ####################
 
 
